c247585990244ca33bb6500e4a945135ad93561f
Marcio Roberto Vasconcelos Jr <marcioj@ciandt.com>
2023-11-29 23:28:18 -0300

    feat/add opening schema package and add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/marciovasconcelosjr/gopportunities/router"
+import (
+	"github.com/marciovasconcelosjr/gopportunities/config"
+	"github.com/marciovasconcelosjr/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+	// Initialize routes
 	router.Initialize()
 }
