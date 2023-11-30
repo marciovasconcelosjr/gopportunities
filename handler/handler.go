f500debdef502c2f204a1fecb90568ed141a3622
Marcio Roberto Vasconcelos Jr <marcioj@ciandt.com>
2023-11-29 22:54:28 -0300

    feat/add separate files for route handlers

--- handler/handler.go
+++ handler/handler.go
@@ -1,13 +1,1 @@
 package handler
-
-import "github.com/gin-gonic/gin"
-
-func CreateOpeningHandler(ctx *gin.Context) {}
-
-func GetOpeningHandler(ctx *gin.Context) {}
-
-func ListOpeningsHandler(ctx *gin.Context) {}
-
-func UpdateOpeningHandler(ctx *gin.Context) {}
-
-func DeleteOpeningHandler(ctx *gin.Context) {}
